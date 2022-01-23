e2e599a39d7d99a49d804fcc4f21267fc68e36a0
Phuc Perseds <hiennhanomoris2001@gmail.com>
2022-01-23 15:59:48 +0700

    add abs_class for asteroid and slove destroy objs when them out of boundary

---
+++ Scripts/Asteroid.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    [SerializeField] private float asteroidSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        move();     
+    }
+
+    void move()
+    {
+        this.transform.Translate(Vector2.down * asteroidSpeed);  
+    }
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(!other.gameObject.CompareTag("background"))
+        {
+            Destroy(this.gameObject);
+            Destroy(other.gameObject);
+        }
+    }
+}
