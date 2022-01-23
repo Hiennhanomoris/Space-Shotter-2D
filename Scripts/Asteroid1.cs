f58ece2691445828d36498742d40cadf4f51b13b
Phuc Perseds <hiennhanomoris2001@gmail.com>
2022-01-23 17:06:20 +0700

    add player bullet

--- Scripts/Asteroid1.cs
+++ Scripts/Asteroid1.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//ke thua tu lop asteroid
 public class Asteroid1 : Asteroid
 {
     // Start is called before the first frame update
