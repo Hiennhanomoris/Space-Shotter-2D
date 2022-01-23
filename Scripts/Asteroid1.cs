1fcaa3bcd9d5deb5eb9eddbed3e27c7528086eb7
Phuc Perseds <hiennhanomoris2001@gmail.com>
2022-01-23 16:14:06 +0700

    add layer for background and objs

--- Scripts/Asteroid1.cs
+++ Scripts/Asteroid1.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DestroyByBoundary : MonoBehaviour
+public class Asteroid1 : Asteroid
 {
     // Start is called before the first frame update
     void Start()
@@ -14,10 +14,5 @@ public class DestroyByBoundary : MonoBehaviour
     void Update()
     {
         
-    }
-
-    void OnTriggerExit2D(Collider2D other) 
-    {
-        Destroy(other.gameObject);    
     }
 }
