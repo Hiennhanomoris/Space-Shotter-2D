95d712dd45f09f273d45dc120027b5ee00619f38
Phuc Perseds <hiennhanomoris2001@gmail.com>
2022-01-25 22:20:35 +0700

    use corotine to spawn asteroid in wave

--- Scripts/Asteroid.cs
+++ Scripts/Asteroid.cs
@@ -8,7 +8,7 @@ public abstract class Asteroid : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
---
+++ Scripts/GameController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    private GameObject enemy1;
+    private GameObject asteroid1;
+    [SerializeField] private float timeWait;
+    [SerializeField] private int numOfasteroid;
+    [SerializeField] private Vector3 pos;
+    private int score;
+
+    public void Awake() 
+    {
+        score = 0;
+        enemy1 = GameObject.FindGameObjectWithTag("enemy1");
+        asteroid1 = GameObject.FindGameObjectWithTag("asteroid1");    
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnWaves());   
+    }
+
+    public IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(timeWait);
+        while(true)
+        {
+            for(int i = 0; i < numOfasteroid; i++)
+            {
+                Vector3 temp = new Vector3(Random.Range(-pos.x, pos.x), pos.y, pos.z);
+                Instantiate(asteroid1, temp, Quaternion.identity);
+            }
+
+            if(score%5 == 0)
+            {
+                Vector3 temp = new Vector3(Random.Range(-pos.x, pos.x), pos.y, pos.z);
+                Instantiate(enemy1, temp, Quaternion.identity);
+            }
+            yield return new WaitForSeconds(timeWait);
+        }
+    }
+
+    public void IncreScore(int point)
+    {
+        score += point;
+    }
+}
